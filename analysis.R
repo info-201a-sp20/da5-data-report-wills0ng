04b5462f6eedaf057d393dd6fa7c92f0ce93f9d9
wsong6 <wsong6@uw.edu>
2020-05-20 23:37:21 -0700

    added comments to both analysis.R and index.Rmd

--- analysis.R
+++ analysis.R
@@ -6,11 +6,6 @@ shootings <- read.csv(file = "data/shootings-2018.csv",
                       stringsAsFactors = FALSE)
 is.data.frame(shootings)
 
-# Some basic stats
-column_names <- colnames(shootings)
-num_shootings <- nrow(shootings)
-tot_killed <- sum(shootings$num_killed)
-
 # Create some new columns we'll use later
 shootings <- shootings %>%
   mutate(date = as.Date(date, "%B %d, %Y"),
@@ -18,7 +13,11 @@ shootings <- shootings %>%
          month_text = format(date, "%B"),
          num_casualties = num_injured + num_killed)
 
-# Define reusable functions
+
+## Define reusable functions
+
+# This function returns the top n groups by total casualties
+# when grouped by the group_by_col
 get_top_n_casualties <- function(group_by_col, n, df) {
   df %>%
     group_by(!!as.name(group_by_col)) %>%
@@ -27,10 +26,19 @@ get_top_n_casualties <- function(group_by_col, n, df) {
     head(n)
 }
 
+# This function returns the top group name by casualty
 get_highest_casualty_group <- function(group_by_col, df) {
   get_top_n_casualties(group_by_col, 1, df) %>%
     pull(!!as.name(group_by_col))
 }
+
+
+## Analysis
+
+# Some basic stats
+column_names <- colnames(shootings)
+num_shootings <- nrow(shootings)
+tot_killed <- sum(shootings$num_killed)
 
 # Below we analyze highest impact by different variables
 # Where highest "impact" means most casuaties
@@ -42,23 +50,25 @@ most_impacted_city <- get_highest_casualty_group("city", shootings)
 most_impacted_state <- get_highest_casualty_group("state", shootings)
 
 # Get most impacted month
-# We need to create the month variable first
-# by extracting the short month name as the first 3 chars of date
 most_impacted_month <- get_highest_casualty_group("month_text", shootings)
 
-
+# Get top 5 cities with highest casualties
+# Also calculate the difference in casualties between the city
+# and the next highest casualty city, and by what factor it's bigger
 top_5_cities <- get_top_n_casualties("city", 5, shootings) %>%
   mutate(diff = tot_casualties - lead(tot_casualties, 1)) %>%
   mutate(factor = round(tot_casualties / lead(tot_casualties, 1), 2))
 
+# Extract just the difference
 top_diff <- top_5_cities %>%
   slice(1) %>%
   pull(diff)
 
+# Extract just the factor
 top_factor <- top_5_cities %>%
   slice(1) %>%
   pull(factor)
 
 
-# Seattle (Skyway) / Renton incident on April 29
+# Get data for just the Seattle (Skyway) / Renton incident on April 29
 selected_incident <- shootings %>% slice(266)
